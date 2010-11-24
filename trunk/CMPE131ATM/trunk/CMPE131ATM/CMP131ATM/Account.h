d733f7f0e049bc3ca3eb0a932a8c34e32914c370
shaun.guth <shaun.guth@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-11-24 03:42:24 +0000

    comment change

---
+++ trunk/CMPE131ATM/trunk/CMPE131ATM/CMP131ATM/Account.h
@@ -0,0 +1,29 @@
+#pragma once
+class Account
+{
+public:
+	Account(void);
+	Account(int customerID);
+
+public:
+	int getCustomerID();
+	void setCustomerID(int customerID);
+
+	double getBalance ();
+	void setBalance(double balance);
+
+	// returns balance after withdrawal or deposit
+	double withdrawFunds(double withdrawalAmount);
+	double depositFunds(double depositAmount);
+
+	// prints information about the account
+	void printSummary();
+
+	// prints the transaction history for the account
+	void printHistory();
+
+protected:
+	int customer_id;
+	double balance;
+
+};
