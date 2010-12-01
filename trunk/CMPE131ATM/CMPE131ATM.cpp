efcf2984f1ee752a3d6479f39eb701a8bf1beb0c
shaun.guth <shaun.guth@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-12-01 00:38:21 +0000

    Factored user-input into a new routine _repeatedly_prompt_user_for_positive_numeric_input Factored database-updating for deposits/withdrawals into a single function Cleaned up the code a little bit.

--- trunk/CMPE131ATM/CMPE131ATM.cpp
+++ trunk/CMPE131ATM/CMPE131ATM.cpp
@@ -11,32 +11,56 @@
 
 using namespace std;
 
-// application functions
+/*
+ * application logical flow functions
+ */
+// handles authenticating the customer number and PIN
 Customer* loginCustomer(sqlite3* dbh);
-static int _db_load_customer(void* customer, int argc, char** argv, char** azColName);
 
+// main menu
 void applicationMenuLoop(Customer* customer, sqlite3* dbh);
+
+// handles deposit/withdrawal user interaction
 void handleDeposit(Customer* customer, sqlite3* dbh);
 void handleWithdrawal(Customer* customer, sqlite3* dbh);
 
+
+/*
+ * utility functions
+ */
+// callback used by sqlite3_exec to populate the given Customer instance
+static int _db_load_customer(void* customer, 
+	int argc, char** argv, char** cols);
+
+void _update_account_balance(sqlite3* dbh, Customer* customer, int balance);
+
+static int _repeatedly_prompt_user_for_positive_numeric_input
+	(const char* prompt, const char* error, int isMoney);
+
+// lets us convert string input to native types we desire
+template <class T>
+bool from_string(T& t, const std::string& s, 
+	std::ios_base& (*f)(std::ios_base&))
+{
+	std::istringstream iss(s);
+	return !(iss >> f >> t).fail();
+}
+
+
+
+/*
+ * globals
+ */
 // the database filename we'll connect to (using sqlite3)
 const char* gDSN = "cmpe131atm.sqlite";
 
 // how many login attempts we allow before exiting
 static const int gLoginAttemptsAllowed = 3;
 
-// lets us convert string input to native types we desire
-template <class T>
-bool from_string(T& t, 
-                 const std::string& s, 
-                 std::ios_base& (*f)(std::ios_base&))
-{
-  std::istringstream iss(s);
-  return !(iss >> f >> t).fail();
-}
 
-
-// entry point for our application
+/*
+ * entry point
+ */
 void main(int argc, _TCHAR* argv[])
 {
 	// connect to the database
@@ -51,7 +75,6 @@ void main(int argc, _TCHAR* argv[])
 		goto exit;
 	}
 
-	// log in a customer
 	Customer* customer = loginCustomer(dbh);
 
 	if (NULL == customer) { // we couldn't log them in
@@ -78,7 +101,7 @@ void main(int argc, _TCHAR* argv[])
 }
 
 
-// prompts the customer to input their credentials and returns a newly instantiated
+// prompts the customer to input their credentials and returns a newly created
 // Customer object if they are successful at logging in.
 Customer* loginCustomer(sqlite3* dbh) {
 	assert(NULL != dbh); // database handle should be initialized 
@@ -87,28 +110,11 @@ Customer* loginCustomer(sqlite3* dbh) {
 get_customer_number: // execution flow returns here if the user fails to auth
 	login_attempts++;
 
-	int customer_id = 0;
-	do {
-		cout << "Please input your customer number: ";
-		string buffer;
-		getline(cin, buffer);
+	int customer_id = _repeatedly_prompt_user_for_positive_numeric_input(
+		"Please input your customer number: ", NULL, 0);
 
-		if (! from_string<int>(customer_id, buffer, std::dec)) {
-			customer_id = 0;
-		}
-	} while(!customer_id);
-
-	//get_security_code:
-	int customer_pin = 0;
-	do {
-		cout << "Please input your security code: ";
-		string buffer;
-		getline(cin, buffer);
-
-		if (! from_string<int>(customer_pin, buffer, std::dec)) {
-			customer_pin = 0;
-		}
-	} while(!customer_pin);
+	int customer_pin = _repeatedly_prompt_user_for_positive_numeric_input(
+		"Please input your security code: ", NULL, 0);
 
 	// validate against database
 	Customer* customer = new Customer();
@@ -146,14 +152,16 @@ get_customer_number: // execution flow returns here if the user fails to auth
 
 
 // callback for database login / loading routine 
-static int _db_load_customer(void* customer, int argc, char** argv, char** azColName) {
+static int _db_load_customer(void* customer, 
+	int argc, char** argv, char** cols)
+{
 	Account* account = new Account();
 
 	// load database row into the account/Customer object
 	// argv = { id, customer_id, customer_pin, balance }
 	account->setAccountID(atoi(argv[0]));
 	((Customer*)customer)->setCustomerID(atol(argv[1]));
-	account->setBalance(atof(argv[3]));
+	account->setBalance(atoi(argv[3]));
 
 	// add account to the customer object, authorize them
 	((Customer*)customer)->setAccount(account);
@@ -163,9 +171,8 @@ static int _db_load_customer(void* customer, int argc, char** argv, char** azCol
 }
 
 
-// main application loop
+// main menu loop
 void applicationMenuLoop(Customer* customer, sqlite3* dbh) {
-
 	menu:
 	cout << "1. Print Account Summary" << endl;
 	cout << "2. View Transaction History" << endl;
@@ -173,20 +180,13 @@ void applicationMenuLoop(Customer* customer, sqlite3* dbh) {
 	cout << "4. Withdraw Funds" << endl;
 	cout << "5. Exit" << endl;
 
-	int menu_choice = 0;
-	do {
-		cout << "Choice? ";
-		string buffer;
-		getline(cin, buffer);
+	int menu_choice = _repeatedly_prompt_user_for_positive_numeric_input(
+		"Choice? ", "Invalid choice.  Try again.", 0);
 
-		if (! from_string<int>(menu_choice, buffer, std::dec)) {
-			menu_choice = 0;
-		}
-
-		if (menu_choice < 1 || menu_choice > 5) {
-			cout << "Invalid choice.  Try again." << endl;
-		}
-	} while(menu_choice < 1 || menu_choice > 5);
+	if (menu_choice < 1 || menu_choice > 5) {
+		cout << "Invalid choice.  Try again." << endl;
+		goto menu;
+	}
 
 	switch (menu_choice) { // options from the menu above
 	case 1:
@@ -212,31 +212,49 @@ void applicationMenuLoop(Customer* customer, sqlite3* dbh) {
 
 // handle gathering user input for a deposit and updating the database
 void handleDeposit(Customer* customer, sqlite3* dbh) {
-	// gather user input
-	int deposit_amount = -1;
-	do {
-		cout << "Enter deposit amount: $";
-		string buffer;
-		getline(cin, buffer);
+	int deposit_amount = _repeatedly_prompt_user_for_positive_numeric_input(
+		"Enter deposit amount: $", "Invalid deposit.  Try again.", 1);
 
-		double temp_deposit_amount;
-		if (! from_string<double>(temp_deposit_amount, buffer, std::dec)) {
-			deposit_amount = -1;
-		}
+	// bail out if they ended up deciding not to deposit anything.
+	if (!deposit_amount) {
+		return;
+	}
 
-		deposit_amount = (int)(temp_deposit_amount * 100);
+	int current_balance = customer->getAccount()->getBalance();
+	_update_account_balance(dbh, customer, current_balance + deposit_amount);
+}
 
-		if (deposit_amount < 0) {
-			cout << "Invalid deposit amount.  Try again." << endl;
-		}
-	} while(deposit_amount < 0);
 
-	// update database
+// handle gathering user input for a withdrawal and updating the database
+void handleWithdrawal(Customer* customer, sqlite3* dbh) {
+	int current_balance = customer->getAccount()->getBalance();
+
+	enter_withdrawal_amount:
+	int withdrawal_amount = _repeatedly_prompt_user_for_positive_numeric_input(
+		"Enter withdrawal amount: $", "Invalid withdrawal.  Try again.", 1);
+
+	// bail out if they ended up deciding not to withdraw anything
+	if (!withdrawal_amount) {
+		return;
+	}
+
+	// check for sufficient funds
+	if (withdrawal_amount > current_balance) {
+		cout << "You do not have sufficient funds.  Try again." << endl;
+		goto enter_withdrawal_amount;
+	}
+
+	_update_account_balance(dbh, customer, current_balance - withdrawal_amount);
+}
+
+
+// handles the database interaction for updating an account balance
+void _update_account_balance(sqlite3* dbh, Customer* customer, int balance) {
 	stringstream sql;
-	sql << "UPDATE accounts SET balance=balance+" << deposit_amount
+	sql << "UPDATE accounts SET balance=" << balance
 		<< " WHERE customer_id=" << customer->getCustomerID();
 	char* sqlite3_error;
-	int rc = sqlite3_exec(dbh, sql.str().c_str(), NULL, customer, &sqlite3_error);
+	int rc = sqlite3_exec(dbh, sql.str().c_str(), NULL, NULL, &sqlite3_error);
 
 	if (SQLITE_OK != rc) {
 		cerr << "Error updating database: " << sqlite3_errmsg(dbh) << endl;
@@ -245,56 +263,44 @@ void handleDeposit(Customer* customer, sqlite3* dbh) {
 	}
 
 	// once the database has been updated, update our account object to match
-	// TODO, should we protect against overflowing a double?
-	int balance = customer->getAccount()->depositFunds(deposit_amount);
+	// TODO, should we protect against overflowing an int?
+	customer->getAccount()->setBalance(balance);
 
 	printf("Your new balance is: $%.2f\n", (float)balance / 100.0f);
 }
 
 
-void handleWithdrawal(Customer* customer, sqlite3* dbh) {
-	// gather user input
-	int withdrawal_amount = -1;
-	int current_balance = customer->getAccount()->getBalance();
-
+// prompts a user to input a numeric value (either floating point or integer)
+// if 'isMoney' is a true value, multiples the user input by 100 and converts
+// the result to an integer.
+static int _repeatedly_prompt_user_for_positive_numeric_input
+	(const char* prompt, const char* error, int isMoney)
+{
+	int input = -1;
 	do {
-		cout << "Enter withdrawal amount: $";
+		cout << prompt;
 		string buffer;
 		getline(cin, buffer);
 
-		double temp_withdrawal_amount;
-		if (! from_string<double>(temp_withdrawal_amount, buffer, std::dec)) {
-			withdrawal_amount = -1;
+		if (isMoney) {
+			double temp_input;
+			if (! from_string<double>(temp_input, buffer, std::dec)) {
+				input = -1;
+			}
+			else {
+				input = (int)(temp_input * 100);
+			}
+		}
+		else {
+			if (! from_string<int>(input, buffer, std::dec)) {
+				input = -1;
+			}
 		}
 
-		withdrawal_amount = (int)(temp_withdrawal_amount * 100);
-
-		if (withdrawal_amount < 0) {
-			cout << "Invalid withdrawal amount.  Try again." << endl;
+		if (input < 0 && NULL != error) {
+			cout << error << endl;
 		}
+	} while(input < 0);
 
-		if (withdrawal_amount > current_balance) {
-			cout << "You do not have sufficient funds.  Try again." << endl;
-			withdrawal_amount = -1;
-		}
-	} while(withdrawal_amount < 0);
-
-	// update database
-	stringstream sql;
-	sql << "UPDATE accounts SET balance=balance-" << withdrawal_amount
-		<< " WHERE customer_id=" << customer->getCustomerID();
-	char* sqlite3_error;
-	int rc = sqlite3_exec(dbh, sql.str().c_str(), NULL, customer, &sqlite3_error);
-
-	if (SQLITE_OK != rc) {
-		cerr << "Error updating database: " << sqlite3_errmsg(dbh) << endl;
-		sqlite3_free(sqlite3_error);
-		return;
-	}
-
-	// once the database has been updated, update our account object to match
-	// TODO, should we protect against overflowing a double?
-	int balance = customer->getAccount()->withdrawFunds(withdrawal_amount);
-
-	printf("Your new balance is: $%.2f\n", (float)balance / 100.0f);
+	return input;
 }
