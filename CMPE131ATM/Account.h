fd5082f9a61725c17e00f4ef4698dcc3df2368c0
shaun.guth <shaun.guth@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-12-04 07:07:59 +0000

    Added a class diagram, fixed the pre-compiled header issue for good. Removed some unused functions from Account class

--- CMPE131ATM/Account.h
+++ CMPE131ATM/Account.h
@@ -11,10 +11,6 @@ public:
 	int getBalance(void);
 	void setBalance(int balance);
 
-	// returns balance after withdrawal or deposit
-	int withdrawFunds(int withdrawalAmount);
-	int depositFunds(int depositAmount);
-
 	// prints information about the account
 	void printSummary();
 
