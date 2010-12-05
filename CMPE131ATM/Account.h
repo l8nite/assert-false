be8f78c5e251786ec979e1d4c7a6b2fd25ba68aa
shaun.guth <shaun.guth@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-12-05 21:51:55 +0000

    Adds doxygen-worthy comments to Account.h Check out the generated version: http://idlemonkeys.net/~shaun/assert-false/CMPE131ATM/html/classAccount.html

--- CMPE131ATM/Account.h
+++ CMPE131ATM/Account.h
@@ -1,29 +1,98 @@
+/**
+ * @file Account.h
+ * @author assert-false@googlegroups.com
+ * @date December 5th, 2010
+ * @brief Account class declaration
+*/
+
 #pragma once
 #include <string>
 #include <iostream>
 
 using namespace std;
 
+/**
+ * @class Account
+ *
+ * The Account class holds information about an individual account record.
+ * This includes the account ID, the current balance, and the account label.
+ *
+ * It also provides a convenient way to print the account summary to stdout.
+ *
+ * Example Usage:
+ * @code
+ *     Account* account = new Account();
+ *
+ *     account->setAccountID(555);
+ *     account->setBalance(5000); // $50
+ *     account->setLabel("Checking");
+ *
+ *     account->printSummary();
+ * @endcode
+ */
 class Account
 {
 public:
+	/**
+	 * @name Account constructor
+	 */
 	Account(void);
 
 public:
+	/** @name Accessor/Mutators
+	 * These are the protected member accessor/mutators
+	 */
+	//@{
+
+	/**
+	 * Gets the account ID
+	 * @return an integer representing the account ID
+	 */
 	int getAccountID(void);
+	
+	/** 
+	 * Sets the account ID
+	 * @param accountID the new accountID to set
+	 */
 	void setAccountID(int accountID);
 
+	/**
+	 * Gets the account balance
+	 * @return an integer representing the account balance in cents
+	 */
 	int getBalance(void);
+
+	/**
+	 * Sets the account balance
+	 * @param balance the account balance in cents
+	 */
 	void setBalance(int balance);
 
+	/**
+	 * Gets the account label
+	 * @return a string for the account label
+	 */
 	string getLabel(void);
-	void setLabel(string label);
 
-	// prints information about the account
+	/**
+	 * Sets the account label
+	 * @param label the new account label
+	 */
+	void setLabel(string label);
+	//@}
+
+	/**
+	 * Prints a one-line summary of the account to stdout
+	 */
 	void printSummary();
 
 protected:
+	//! The account ID
 	int account_id;
+
+	//! The current balance (in cents)
 	int balance;
+
+	//! The account label (ie., "Checking" or "Savings")
 	string label;
 };
