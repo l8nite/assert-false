f1d81c0712e5284fee39dc310fbcc962992f4754
Dingodongo1@gmail.com <Dingodongo1@gmail.com@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-12-05 20:49:46 +0000

    Commenting update... not complete

--- CMPE131ATM/Account.h
+++ CMPE131ATM/Account.h
@@ -1,20 +1,42 @@
 #pragma once
+//Acccount -- contains account information for each customer.
+//@author Assert-False
+
 class Account
 {
 public:
+	//class constructor
+	//@param none
+	//@return none
 	Account(void);
 
 public:
+	//gets AccountID value
+	//@param none
+	//@retun AccountID
 	int getAccountID(void);
+
+	//sets AccountID value
+	//@param accountID
+	//@return none
 	void setAccountID(int accountID);
 
+	//gets balance value
+	//@param none
+	//@retun balance
 	int getBalance(void);
+
+	//sets balance value
+	//@param balance
+	//@return none
 	void setBalance(int balance);
 
-	// prints information about the account
+	//prints account information to the screen
+	//@param none
+	//@return none
 	void printSummary();
 
 protected:
 	int account_id;
-	int balance;
+	int balance; //balance: kept in integer form. $1000.00 is equal to 100000
 };
--- CMPE131ATM/Customer.h
+++ CMPE131ATM/Customer.h
@@ -8,18 +8,33 @@ public: // constructors
 	virtual ~Customer(void);
 
 public: // instance methods
+
 	Account* getAccount(void);
 	void setAccount(Account* account);
 
+	//gets customerid value
+	//@param none
+	//@retun customerid 
 	int getCustomerID(void);
+
+	//sets customerid value
+	//@param customerid
+	//@return none
 	void setCustomerID(int customerID);
 
+	//gets isAuthorized value
+	//@param none
+	//@retun isAuthorized 
 	int getIsAuthorized();
+
+	//sets isAuthorized value
+	//@param isAuthorized
+	//@return none
 	void setIsAuthorized(int isAuthorized);
 
 protected: // member variables
-	int customer_id;
-	int is_authorized;
+	int customer_id;   //customer_id: id of customer
+	int is_authorized; //is_authorized: value of authorization during login
 
 	Account* account; // TODO: more than one account
 };
