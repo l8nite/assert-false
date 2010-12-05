3cbf8bdff0ae2f5c95c64b826e6664804a537261
shaun.guth <shaun.guth@276f937a-2cbd-45fe-29b1-af90861f0d0c>
2010-12-05 21:19:10 +0000

    Iteration 2 - Adds support for multiple accounts I added a new menu screen that lets a user choose the active account. I'm reverting John's commenting changes temporarily, since they conflicted with my change.

--- CMPE131ATM/Account.h
+++ CMPE131ATM/Account.h
@@ -1,42 +1,29 @@
 #pragma once
-//Acccount -- contains account information for each customer.
-//@author Assert-False
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 class Account
 {
 public:
-	//class constructor
-	//@param none
-	//@return none
 	Account(void);
 
 public:
-	//gets AccountID value
-	//@param none
-	//@retun AccountID
 	int getAccountID(void);
-
-	//sets AccountID value
-	//@param accountID
-	//@return none
 	void setAccountID(int accountID);
 
-	//gets balance value
-	//@param none
-	//@retun balance
 	int getBalance(void);
-
-	//sets balance value
-	//@param balance
-	//@return none
 	void setBalance(int balance);
 
-	//prints account information to the screen
-	//@param none
-	//@return none
+	string getLabel(void);
+	void setLabel(string label);
+
+	// prints information about the account
 	void printSummary();
 
 protected:
 	int account_id;
-	int balance; //balance: kept in integer form. $1000.00 is equal to 100000
+	int balance;
+	string label;
 };
--- CMPE131ATM/Customer.h
+++ CMPE131ATM/Customer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Account.h"
+#include <vector>
 
 class Customer
 {
@@ -8,34 +9,18 @@ public: // constructors
 	virtual ~Customer(void);
 
 public: // instance methods
+	vector<Account*> getAccounts(void);
+	void addAccount(Account* account);
 
-	Account* getAccount(void);
-	void setAccount(Account* account);
-
-	//gets customerid value
-	//@param none
-	//@retun customerid 
 	int getCustomerID(void);
-
-	//sets customerid value
-	//@param customerid
-	//@return none
 	void setCustomerID(int customerID);
 
-	//gets isAuthorized value
-	//@param none
-	//@retun isAuthorized 
 	int getIsAuthorized();
-
-	//sets isAuthorized value
-	//@param isAuthorized
-	//@return none
 	void setIsAuthorized(int isAuthorized);
 
 protected: // member variables
-	int customer_id;   //customer_id: id of customer
-	int is_authorized; //is_authorized: value of authorization during login
-
-	Account* account; // TODO: more than one account
+	int customer_id;
+	int is_authorized;
+	vector<Account*> accounts;
 };
 
